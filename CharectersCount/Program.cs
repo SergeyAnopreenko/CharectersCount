3f14cddaa3c0a94b84a2e39aa5121eaa5fbeec71
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-01-31 15:57:39 +0200

    Add function is null or empty

--- CharectersCount/Program.cs
+++ CharectersCount/Program.cs
@@ -4,6 +4,12 @@ Console.WriteLine("Enter the symbols you want to count");
 
 var userInput = Console.ReadLine();
 
+if (string.IsNullOrEmpty(userInput))
+{ 
+    Console.WriteLine("You didn't enter any symbols, try again");
+    return;
+}
+
 CharacterCounter characterCounter = new CharacterCounter(userInput);
 
 Dictionary<char, int> sorter = characterCounter.LettersSorter();
