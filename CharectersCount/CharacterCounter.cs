22f99bd6ea455c781d258fcee737ecd18aee0ed9
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-01-31 15:24:31 +0200

    Add the methods

---
+++ CharectersCount/CharacterCounter.cs
@@ -0,0 +1,37 @@
+﻿namespace CountCharacters
+{
+    public class CharacterCounter
+    {
+        private readonly string symbolsInput;
+
+        public CharacterCounter(string userInput)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(userInput);
+
+            symbolsInput = userInput.ToLower();
+        }
+
+        public Dictionary<char, int> LettersSorter()
+        {
+            return LettersCounter()
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private Dictionary<char, int> LettersCounter()
+        {
+            Dictionary<char, int> diction = new();
+
+            foreach (char c in symbolsInput)
+            {
+                if (char.IsLetter(c))
+
+                    if (diction.TryGetValue(c, out int value))
+                        diction[c] = ++value;
+                    else
+                        diction[c] = 1;
+            }
+            return diction;
+        }
+    }
+}
--- CharectersCount/Program.cs
+++ CharectersCount/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using CountCharacters;
+
+Console.WriteLine("Enter the characters you want to count");
+
+var userInput = Console.ReadLine();
+
+CharacterCounter characterCounter = new CharacterCounter(userInput);
+
+Dictionary<char, int> sorter = characterCounter.LettersSorter();
+
+foreach (var pair in sorter)
+{
+    Console.WriteLine($"{pair.Key} : {pair.Value}");
+}
